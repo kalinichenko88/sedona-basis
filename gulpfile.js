e14cad9c505db3b5c6a3e91d6cf0f430f05f6964
Maxim Sofronov <imaxsof@gmail.com>
2018-05-04 12:51:12 +0300

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ const paths = {
 				'node_modules/normalize.css/normalize.css'
 			],
 			main: [
+				'node_modules/sedona-blocks/grid/grid.less',
 				'node_modules/sedona-blocks/scene/scene.less',
 				'node_modules/sedona-blocks/wysiwyg/wysiwyg.less',
 				'src/less/main.less'
@@ -59,6 +60,7 @@ const paths = {
 	},
 	watch: {
 		html: [
+			'src/blocks/**/*.html',
 			'src/inc/*.html',
 			'src/*.html'
 		],
